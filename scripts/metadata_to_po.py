85048ab62e4f207284728d004f726e40657edd58
Jason P. Pickering <jason.p.pickering@gmail.com>
2016-08-15 15:38:10 +0200

    Refactor to use translatable objects file.

--- scripts/metadata_to_po.py
+++ scripts/metadata_to_po.py
@@ -38,52 +38,8 @@ def save_obj_to_pofile(obj,file_name):
 
 
 def get_objs_to_translate():
-    meta_objs = [{'dataElements':'name'},
-                 {'dataElements':'shortName'},
-                 {'organisationUnits':'name'},
-                 {'organisationUnits':'shortName'},
-                 {'dataSets':'name'},
-                 {'dataSets' : 'shortName'},
-                 {'optionSets' : 'name' },
-                 {'options' : 'name'},
-                 {'organisationUnitLevels': 'name'},
-                 {'organisationUnitGroups' : 'name'},
-                 {'organisationUnitGroups' : 'shortName'},
-                 {'organisationUnitGroups' : 'description'},
-                 {'organisationUnitGroupSets' : 'name'},
-                 {'organisationUnitGroupSets' : 'shortName'},
-                 {'organisationUnitGroupSets' : 'description'},
-                 {'categoryOptions' : 'name'},
-                 {'categoryOptions' : 'shortName'},
-                 {'categoryOptions' : 'description'},
-                 {'categories':'name'},
-                 {'categories':'shortName'},
-                 {'categories':'description'},
-                 {'categoryCombos':'name'},
-                 {'categoryOptionGroups':'name'},
-                 {'categoryOptionGroups':'shortName'},
-                 {'categoryOptionGroups':'description'},
-                 {'categoryOptionGroupSets':'name'},
-                 {'categoryOptionGroupSets':'shortName'},
-                 {'categoryOptionGroupSets':'description'},
-                 {'indicators':'name'},
-                 {'indicators': 'shortName'},
-                 {'validationRules':'name'},
-                 {'programs':'name'},
-                 {'programs':'shortName'},
-                 {'trackedEntityAttributes':'name'},
-                 {'trackedEntityAttributes':'shortName'},
-                 {'programStages':'name'},
-                 {'programRules':'name'},
-                 {'programRules':'description'},
-                 {'dataElementGroups':'name'},
-                 {'dataElementGroups':'shortName'},
-                 {'dataElementGroupSets':'name'},
-                 {'dataElementGroupSets':'shortName'},
-                 {'indicatorTypes':'name'},
-                 {'indicatorGroups':'name'} ]
-
-    return meta_objs
+    json_data=open('./translatable_objects.json').read()
+    return json.loads(json_data)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Convert DHIS2 metadata to PO files')
@@ -97,20 +53,23 @@ def main():
     if not os.path.isfile(local_config):
         print("Warning: The credentials file "+local_config+" doesn't seem to exist, so this might not work.")
     api=Server(Config(local_config))
-    meta_objs = get_objs_to_translate()
+    objs = get_objs_to_translate()
 
-    for obj in meta_objs:
-        this_object =  list( obj.keys() )[0]
-        this_property = list( obj.values() )[0]
-        this_obj = get_translatable_object(api, this_object, this_property)
-        if  len(this_obj) > 0 :
-            this_po = obj_to_pofile(this_obj, this_property)
-            file_to_save = this_object + "_" + this_property + ".pot"
-            if not os.path.exists("../pofiles/" + this_object):
-                os.makedirs("../pofiles/" + this_object)
-            this_po.save("../pofiles/" + this_object + "/" + file_to_save)
-        else:
-            print("No " + this_property +  " to translate for " + this_object + ".Skipping.")
+    for obj in objs:
+        i=0
+        while i < len( obj.values() ):
+            this_object =  list( obj.keys() )[0]
+            this_property = list( obj.values() )[0][i]
+            i += 1
+            this_obj = get_translatable_object(api, this_object, this_property)
+            if len(this_obj) > 0 :
+                this_po = obj_to_pofile(this_obj, this_property)
+                file_to_save = this_object + "_" + this_property + ".pot"
+                if not os.path.exists("../pofiles/" + this_object):
+                    os.makedirs("../pofiles/" + this_object)
+                this_po.save("../pofiles/" + this_object + "/" + file_to_save)
+            else:
+                print("No " + this_property +  " to translate for " + this_object + ".Skipping.")
 
 if __name__ == "__main__":
     main()
